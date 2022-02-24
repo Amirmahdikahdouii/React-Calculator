958d45c419f20a3d2ede64956e07d7fd9cf5bad9
Amir Mahdi Kahdouii <Amirmahdikahdouii@users.noreply.github.com>
2022-02-24 22:14:38 +0330

    Add html and css to the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    return (
+        <div class="container">
+            <div class="out-put">
+                <div class="previous-answer">
+                    <label id="previous-answer-label">255</label>
+                </div>
+                <div class="answer">
+                    <label id="answer-label">255</label>
+                </div>
+            </div>
+            <div class="buttons-container">
+                <div class="row">
+                    <button class="btn btn-danger">AC</button>
+                    <button class="btn btn-danger">DEL</button>
+                    <button class="btn btn-success">-</button>
+                </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                <div class="row">
+                    <button class="btn btn-dark">7</button>
+                    <button class="btn btn-dark">8</button>
+                    <button class="btn btn-dark">9</button>
+                    <button class="btn btn-success">*</button>
+                </div>
+
+                <div class="row">
+                    <button class="btn btn-dark">4</button>
+                    <button class="btn btn-dark">5</button>
+                    <button class="btn btn-dark">6</button>
+                    <button class="btn btn-success">/</button>
+                </div>
+
+                <div class="row">
+                    <button class="btn btn-dark">1</button>
+                    <button class="btn btn-dark">2</button>
+                    <button class="btn btn-dark">3</button>
+                    <button class="btn btn-success">+</button>
+                </div>
+
+                <div class="row row-last">
+                    <button class="btn btn-warning">.</button>
+                    <button class="btn btn-warning">=</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
